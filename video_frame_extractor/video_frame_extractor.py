f924a93977adcf7d27cce211ca4f95e0e074c615
olecram <marcelo.acosta1994@gmail.com>
2022-02-08 20:02:41 -0400

    Added git ignore file

--- video_frame_extractor/video_frame_extractor.py
+++ video_frame_extractor/video_frame_extractor.py
@@ -3,35 +3,41 @@ from datetime import timedelta
 import cv2
 import numpy as np
 import os
+import time
 
-SAVING_FRAMES_PER_SECOND = 6
-
-def format_timedelta(td):
+def format_timedelta(time_delta):
     """Utility function to format timedelta objects in a cool way (e.g 00:00:20.05) 
     omitting microseconds and retaining milliseconds"""
-    result = str(td)
+    result = str(time_delta)
     try:
         result, ms = result.split(".")
     except ValueError:
-        return result + ".00".replace(":", "-")
+        return result + ".00"#.replace(":", "-")
     ms = int(ms)
     ms = round(ms / 1e4)
-    return f"{result}.{ms:02}".replace(":", "-")
+    return f"{result}.{ms:02}"#.replace(":", "-")
 
 def get_frames(cap, saving_fps):
     s = []
-    print(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    print(cap.get(cv2.CAP_PROP_FPS))
-    clip_duration = cap.get(cv2.CAP_PROP_FRAME_COUNT) / cap.get(cv2.CAP_PROP_FPS)
-    for i in np.arange(0, clip_duration, 1 / saving_fps):
+    frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    video_fps = cap.get(cv2.CAP_PROP_FPS)
+    print(f"Number of 'real' frames: {frame_count}")
+    print(f"Video fps: {video_fps}")
+    clip_duration = frame_count / video_fps
+    print(f"Video duration: {clip_duration } seg")
+    frame_interval = 1 / saving_fps
+    print(f"Extracting a frame every: {frame_interval} seg")
+    for i in np.arange(0, clip_duration, frame_interval):
         s.append(i)
     return s
 
-def main(video_file):
+def main(video_file, frames_per_second=6):
+    start_time = time.time()
     print(f"Extracting frames of video file: {video_file}");
+    print(f"Frames per second: {frames_per_second}");
     filename, _ = os.path.splitext(video_file)
     file_raw_name, _ = os.path.splitext(os.path.basename(video_file))
-    #filename += "-opencv"
+    filename += "- frames"
     if not os.path.isdir(filename):
         print(f"Creating directory: {filename}")
         os.mkdir(filename)
@@ -40,9 +46,11 @@ def main(video_file):
     
     cap = cv2.VideoCapture(video_file)
     fps = cap.get(cv2.CAP_PROP_FPS)
-    saving_frames_per_second = min(fps, SAVING_FRAMES_PER_SECOND)
+    saving_frames_per_second = min(fps, frames_per_second)
     saving_frames_durations = get_frames(cap, saving_frames_per_second)
-    count,frame_count = 0,0
+    calculated_frames = len(saving_frames_durations)
+    print("Number of frames calculated: " + str(calculated_frames))
+    count,extracted_frame_count = 0,0
     while True:
         is_read, frame = cap.read()
         if not is_read:
@@ -57,12 +65,12 @@ def main(video_file):
             # the list is empty, all duration frames were saved
             break
         if frame_duration >= closest_duration:
-            frame_count += 1
+            extracted_frame_count += 1
             # if closest duration is less than or equals the frame duration, 
             # then save the frame
             frame_duration_formatted = format_timedelta(timedelta(seconds=frame_duration))
-            print(f"Extracted frame {frame_count}")
-            cv2.imwrite(os.path.join(filename, f"Shield Hero - S01E01 - frame {frame_count}.jpg"), frame) 
+            print(f"Extracted frame {extracted_frame_count} / {calculated_frames} at time: {frame_duration_formatted}")
+            cv2.imwrite(os.path.join(filename, f"{file_raw_name} - frame {extracted_frame_count}.jpg"), frame) 
             # drop the duration spot from the list, since this duration spot is already saved
             try:
                 saving_frames_durations.pop(0)
@@ -70,13 +78,24 @@ def main(video_file):
                 pass
         # increment the frame count
         count += 1
-    print("Frames extracted!")
+    stop_time = time.time()
+    ellapsed_time_ms = (stop_time - start_time) * 1000
+    print(f"Frames extracted! - ellapsed time: {ellapsed_time_ms} ms")
 
 if __name__ == '__main__':
     import sys
     video_file = sys.argv[1]
-    print(cv2.__version__)
-    if(os.path.isfile(video_file)):
-        main(video_file)        
-    else:
+    if(not os.path.isfile(video_file)):
         print("No file found")
+
+    arguments = len(sys.argv)
+    if(arguments==2):
+        main(video_file)
+    elif(arguments==3):
+        frames_per_second = float(sys.argv[2])
+        main(video_file, frames_per_second)
+    else:
+        print("too many arguments")
+
+
+
